bb4e1fb97e0ef5fa90ee0cb98162ecf81eca23c3
BGorson <BGorson32@gmail.com>
2019-02-24 12:56:43 -0600

    updated login feature

--- greatbay.js
+++ greatbay.js
@@ -1,6 +1,6 @@
-var inquirer = require("inquirer")
-var mysql = require("mysql");
-var connection = mysql.createConnection({
+const inquirer = require("inquirer")
+const mysql = require("mysql");
+const connection = mysql.createConnection({
   host: "localhost",
 
   // Your port; if not 3306
@@ -14,57 +14,137 @@ var connection = mysql.createConnection({
   database: "greatbayDB"
 });
 
+function initialize(){
+  console.log("*** Welcome to Greatbay! ***")
+  inquirer.prompt([
+    {
+      type:"list",
+      message:"Which would you like to do?",
+      choices: ["Create a user","Login"],
+      name: "selection"
+  }
+]).then(function(response){
+  if (response.selection == "Login"){
+    login();
+  }
+  if (response.selection == "Create a user"){
+    accountCreate();
+  }
+})
+}
+
+function accountCreate(){
+  console.log("pending functionality")
+  inquirer.prompt([{
+    type: "input",
+    message: "What is your username?",
+    name: "newUser"
+  },
+  {
+  type:"password",
+  message:"Enter your password",
+  name: "newPassword"
+  }
+])
+  .then(function (response) {
+    connection.query(
+      "INSERT INTO usernames SET ?", {
+        userName: response.newUser,
+        userPassword: response.newPassword
+      }
+    );
+    console.log("Successfully added")
+    login();
+  })
+  
+}
+
+function login(){
+  inquirer.prompt([{
+    type: "input",
+    message: "What is your username?",
+    name: "user"
+  },
+  {
+  type:"password",
+  message:"Enter your password",
+  name: "password"
+  }
+])
+  .then(function (response) {
+    console.log(response.password + "pass")
+    console.log(response.user + "user name")
+    connection.query(
+      "select userPassword from usernames WHERE ?", {
+      userName: response.user
+      },function (err, res){   
+        if (res[0].userPassword== response.password){
+          console.log("Success!")
+          loginSuccess()
+        }
+
+      })
+  })
+}
+
+function loginSuccess() {
+
 inquirer.prompt([{
     type: "list",
     message: "Which do you want to do?",
-    choices: ["Post", "Bid"],
+    choices: ["Post", "Bid", "Remove", "Exit",],
     name: "userAction"
   }])
   .then(function (response) {
     if (response.userAction == 'Post') {
+      console.log("connected as id " + connection.threadId + "\n");
       postItems();
     }
     if (response.userAction == "Bid") {
       connection.connect(function (err) {
         if (err) throw err;
         console.log("connected as id " + connection.threadId + "\n");
-
         viewItems();
-
       });
     }
+    if (response.userAction == "Remove"){
+      console.log("connected as id " + connection.threadId + "\n");
+      deleteItems();
+    }
+    if (response.userAction == "Exit"){
+      connection.end();
+    }
   })
-
-var highest;
-var itemBid;
-
+}
 function viewItems() {
   connection.query("SELECT * FROM items", function (err, res) {
     if (err) throw err;
-    var itemArray = [];
-    for (i = 0; i < res.length; i++) {
-      itemArray.push(res[i].item_name)
-    }
+    
     inquirer.prompt([{
       type: "list",
       message: "Here are all the items\n",
-      choices: itemArray,
+      choices: function(){
+        let itemArray = [];
+        for (i = 0; i < res.length; i++) {
+          itemArray.push(res[i].item_name)
+        }
+        return itemArray;
+      },
       name: "itemChoice"
 
     }]).then(function (response) {
-      itemBid= response.itemChoice
-      highest= res[0].highest_bid
-      console.log(highest)
-      console.log("You picked "+ itemBid)
-
-      bidItems(itemBid,highest)
+      console.log(response)
+      connection.query("SELECT * FROM items WHERE?",{
+        item_name: response.itemChoice
+      }, function (err, res) {
+        bidItems(response.itemChoice, res[0].highest_bid)
+      })
     })
   });
 }
+function bidItems(item, itemHighest) {
 
-function bidItems(item,itemHighest) {
-
-  console.log("This should be highest Bid "+ itemHighest);
+  console.log("This should be highest Bid " + itemHighest);
   console.log("You're bidding on " + item)
   inquirer.prompt([{
     type: "input",
@@ -82,9 +162,7 @@ function bidItems(item,itemHighest) {
           }
         ],
         function (err, res) {
-          console.log("You got the highest bid on" + item);
-          // Call deleteProduct AFTER the UPDATE completes
-
+          console.log("You got the highest bid on " + item);
         }
       );
     } else {
@@ -95,38 +173,67 @@ function bidItems(item,itemHighest) {
 
 }
 function postItems() {
-  inquirer.prompt([
-    {
-      type:"input",
-      message:"What item do you want to list?",
+  inquirer.prompt([{
+      type: "input",
+      message: "What item do you want to list?",
       name: "name"
     },
     {
-      type:"input",
-      message:"What is the starting bid?",
+      type: "input",
+      message: "What is the starting bid?",
       name: "bid"
     },
     {
-    type:"input",
-    message: "what category is this item in?",
-    name: "category"
+      type: "input",
+      message: "what category is this item in?",
+      name: "category"
     }
-  ]).then(function(response){
-    createListing(response.category,response.name,response.bid)
-})
+  ]).then(function (response) {
+    createListing(response.category, response.name, response.bid)
+  })
 }
-function createListing(category,name,bid) {
+function createListing(category, name, bid) {
   console.log("Inserting a new item...\n");
   connection.query(
-    //replaces the ? with the object identified afterwards
-    "INSERT INTO items SET ?",
-    {
+    "INSERT INTO items SET ?", {
       item_name: name,
       category: category,
-      highest_bid:bid,
-      starting_bid:bid
+      highest_bid: bid,
+      starting_bid: bid
     }
   );
-
   connection.end();
 }
+function deleteItems(){
+  connection.query("SELECT * FROM items", function (err, res) {
+    if (err) throw err;
+    
+    inquirer.prompt([{
+      type: "list",
+      message: "Here are all the items\n",
+      choices: function(){
+        let itemArray = [];
+        for (i = 0; i < res.length; i++) {
+          itemArray.push(res[i].item_name)
+        }
+        return itemArray;
+      },
+      name: "itemChoice"
+
+    }]).then(function (response) {
+      connection.query(
+        "DELETE FROM items WHERE ?",
+        {
+        item_name: response.itemChoice
+        }
+      )
+      console.log("Removed " + response.itemChoice)
+      connection.end();
+   
+    })
+  });
+}
+
+
+
+initialize();
